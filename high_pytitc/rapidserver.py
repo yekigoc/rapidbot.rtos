3e1dc60a6e3d8073b7e8da643b5e234f43bcf501
snegovick <snegovick@snegovick-desktop>
2010-01-02 20:17:48 +0300

    moved socket code to separate thread

--- high_pytitc/rapidserver.py
+++ high_pytitc/rapidserver.py
@@ -16,6 +16,77 @@ class rapidtcphandler(SocketServer.BaseRequestHandler):
     override the handle() method to implement communication to the
     client.
     """
+    def recvpress(self):
+#set dutycycles
+        self.data = self.request.recv(1)
+        tmp = struct.unpack("<B", self.data)
+#        packet = struct.pack("B", 255)
+#        self.request.send(packet)
+        print tmp[0]
+        if (tmp[0]==101):
+        #forward
+            self.server.velmult=-1.0
+        if (tmp[0]==103):
+        #reverse
+            self.server.velmult=1.0
+        if (tmp[0]==100):
+        #left
+            self.server.steermult=-1.0
+        if (tmp[0]==102):
+        #right
+            self.server.steermult=1.0
+        if (tmp[0]==49):
+            self.server.lights[0]=1
+            self.server.lightsapply[0]=1
+        if (tmp[0]==50):
+            self.server.lights[1]=1
+            self.server.lightsapply[1]=1
+        if (tmp[0]==51):
+            self.server.lights[2]=1
+            self.server.lightsapply[2]=1
+
+
+    def recvrelease(self):
+#set dutycycles
+        self.data = self.request.recv(1)
+        tmp = struct.unpack("<B", self.data)
+#        packet = struct.pack("B", 255)
+#        self.request.send(packet)
+        if (tmp[0]==101):
+        #forward
+            self.server.velmult=0.0
+            self.server.velocity=self.server.defvelocity
+        if (tmp[0]==103):
+        #reverse
+            self.server.velmult=0.0
+            self.server.velocity=self.server.defvelocity
+        if (tmp[0]==100):
+        #left
+            self.server.steermult=0.0
+            self.server.steerangle=self.server.defsteerangle
+        if (tmp[0]==102):
+        #right
+            self.server.steermult=0.0
+            self.server.steerangle=self.server.defsteerangle
+        if (tmp[0]==49):
+            self.server.lights[0]=0
+            self.server.lightsapply[0]=1
+        if (tmp[0]==50):
+            self.server.lights[1]=0
+            self.server.lightsapply[1]=1
+        if (tmp[0]==51):
+            self.server.lights[2]=0
+            self.server.lightsapply[2]=1
+
+    def sendtelemetry(self):
+        i = 0
+        packet = struct.pack("<B", 254)
+        self.request.send(packet)
+        while i<8:
+            packet = struct.pack("I", self.server.telemetry[i])
+            self.request.send(packet)
+            i=i+1
+
 
     def handle(self):
         # self.request is the TCP socket connected to the client
@@ -33,46 +104,11 @@ class rapidtcphandler(SocketServer.BaseRequestHandler):
 #            print ("header = " + str(hdr))
             
             if hdr[0] == 1:
-            #set dutycycles
-                self.data = self.request.recv(1)
-                tmp = struct.unpack("<B", self.data)
-                packet = struct.pack("B", 255)
-                self.request.send(packet)
-                if (tmp[0]==101):
-        #forward
-                    self.server.velmult=-1.0
-                if (tmp[0]==103):
-        #reverse
-                    self.server.velmult=1.0
-                if (tmp[0]==100):
-        #left
-                    self.server.steermult=-1.0
-                if (tmp[0]==102):
-        #right
-                    self.server.steermult=1.0
+                self.recvpress()
             if hdr[0] == 2:
-            #set dutycycles
-                self.data = self.request.recv(1)
-                tmp = struct.unpack("<B", self.data)
-                packet = struct.pack("B", 255)
-                self.request.send(packet)
-                if (tmp[0]==101):
-        #forward
-                    self.server.velmult=0.0
-                    self.server.velocity=self.server.defvelocity
-                if (tmp[0]==103):
-        #reverse
-                    self.server.velmult=0.0
-                    self.server.velocity=self.server.defvelocity
-                if (tmp[0]==100):
-        #left
-                    self.server.steermult=0.0
-                    self.server.steerangle=self.server.defsteerangle
-                if (tmp[0]==102):
-        #right
-                    self.server.steermult=0.0
-                    self.server.steerangle=self.server.defsteerangle
-
+                self.recvrelease()
+            if hdr[0] == 3:
+                self.sendtelemetry()
 
 
 class rapidtcpserver(SocketServer.TCPServer):
@@ -88,6 +124,9 @@ class rapidtcpserver(SocketServer.TCPServer):
         self.steerangle=self.defsteerangle
         self.exitflag = 0
         self.allow_reuse_address = True
+        self.lights=[0,0,0]
+        self.lightsapply=[0,0,0]
+        self.telemetry=[0,0,0,0,0,0,0,0]
 
     def init(self, ui=None, port = 30000):
 
@@ -117,7 +156,18 @@ def main():
         dev.set_configuration()
 
 #        CTRL_OUT = [0x81, 0x0]
-        dev.ctrl_transfer(0x01, 0x0, 0x07, 0x0, [0])
+        led = struct.pack("BB", 1, 1)
+        dev.ctrl_transfer(0x01, 0x0, 0x07, 0x0, led)
+        time.sleep(0.2)
+        led = struct.pack("BB", 1, 0)
+        dev.ctrl_transfer(0x01, 0x0, 0x07, 0x0, led)
+        time.sleep(0.2)
+        led = struct.pack("BB", 1, 1)
+        dev.ctrl_transfer(0x01, 0x0, 0x07, 0x0, led)
+        time.sleep(0.2)
+        led = struct.pack("BB", 1, 0)
+        dev.ctrl_transfer(0x01, 0x0, 0x07, 0x0, led)
+
 
         while(True):
             server.velocity = server.velocity+server.velmult*server.acceleration
@@ -130,19 +180,65 @@ def main():
                 server.steerangle = 500
             if(server.steerangle < 293):
                 server.steerangle = 293
-                
-            print ("angle = "+str(server.steerangle) + " velocity = " + str(server.velocity))
-            #            vel = array.array("B", [int(server.velocity)])
-            #            print (vel)
+
+#            print ("angle = "+str(server.steerangle) + " velocity = " + str(server.velocity))
+
             vel = struct.pack("H", server.velocity)
             ang = struct.pack("H", server.steerangle)
-            #            print ("vel = " + str(vel))
-            #            print("len(vel)" + str(len(vel)))
+
             ret = dev.ctrl_transfer(0x01, 0x0, 0x05, 0, vel);
             ret = dev.ctrl_transfer(0x01, 0x0, 0x02, 0, ang);
+
+            i=0
+            while i<len(server.lightsapply):
+                if server.lightsapply[i] == 1:
+                    server.lightsapply[i] = 0
+                    led = struct.pack("BB", i, server.lights[i])
+                    dev.ctrl_transfer(0x01, 0x0, 0x07, 0x0, led)
+                i=i+1
+
+
+#telemetry code
+            adc = dev.ctrl_transfer(0x81, 0x0, 0x08, 0x0, 4*8)
+            adcv1 = struct.pack("BBBB", adc[0], adc[1], adc[2], adc[3])
+            adcv2 = struct.pack("BBBB", adc[4], adc[5], adc[6], adc[7])
+            adcv3 = struct.pack("BBBB", adc[8], adc[9], adc[10], adc[11])
+            adcv4 = struct.pack("BBBB", adc[12], adc[13], adc[14], adc[15])
+            adcv5 = struct.pack("BBBB", adc[16], adc[17], adc[18], adc[19])
+            adcv6 = struct.pack("BBBB", adc[20], adc[21], adc[22], adc[23])
+            adcv7 = struct.pack("BBBB", adc[24], adc[25], adc[26], adc[27])
+            adcv8 = struct.pack("BBBB", adc[28], adc[29], adc[30], adc[31])
+            adcv1 = struct.unpack("I", adcv1)
+            adcv2 = struct.unpack("I", adcv2)
+            adcv3 = struct.unpack("I", adcv3)
+            adcv4 = struct.unpack("I", adcv4)
+            adcv5 = struct.unpack("I", adcv5)
+            adcv6 = struct.unpack("I", adcv6)
+            adcv7 = struct.unpack("I", adcv7)
+            adcv8 = struct.unpack("I", adcv8)
+            server.telemetry[0]=adcv1[0]
+            server.telemetry[1]=adcv2[0]
+            server.telemetry[2]=adcv3[0]
+            server.telemetry[3]=adcv4[0]
+            server.telemetry[4]=adcv5[0]
+            server.telemetry[5]=adcv6[0]
+            server.telemetry[6]=adcv7[0]
+            server.telemetry[7]=adcv8[0]
+
             time.sleep(0.001)
 
     except KeyboardInterrupt:
+        led = struct.pack("BB", 0, 1)
+        dev.ctrl_transfer(0x01, 0x0, 0x07, 0x0, led)
+        time.sleep(0.2)
+        led = struct.pack("BB", 0, 0)
+        dev.ctrl_transfer(0x01, 0x0, 0x07, 0x0, led)
+        time.sleep(0.2)
+        led = struct.pack("BB", 0, 1)
+        dev.ctrl_transfer(0x01, 0x0, 0x07, 0x0, led)
+        time.sleep(0.2)
+        led = struct.pack("BB", 0, 0)
+        dev.ctrl_transfer(0x01, 0x0, 0x07, 0x0, led)
         server.shutdown()
 
 if __name__ == "__main__":
