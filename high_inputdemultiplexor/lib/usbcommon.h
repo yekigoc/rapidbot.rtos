bd9832a04a0ab7a62c73b151bec0ac26930fed8f
snegovick <snegovick@snegovick-desktop>
2010-03-04 04:12:51 +0300

    added state parameters, added usbscheduler

---
+++ high_inputdemultiplexor/lib/usbcommon.h
@@ -0,0 +1,12 @@
+#ifndef __USBCOMMON_H__
+#define __USBCOMMON_H__
+
+#define CTRL_IN			(0x01 | LIBUSB_ENDPOINT_IN)
+#define CTRL_OUT		(0x01 | LIBUSB_ENDPOINT_OUT)
+#define USB_RQ_STAT			0x0
+#include <libusb.h>
+
+#define PER_PWM                 0x1
+#define PER_ADC                 0x2
+
+#endif// __USBCOMMON_H__
