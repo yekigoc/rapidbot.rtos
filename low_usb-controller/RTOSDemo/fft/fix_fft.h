153c678f72e51183332f1fe8bde860933df6ab44
snegovick <snegovick@snegovick-laptop>
2010-03-11 23:53:35 +0300

    added locator and fft code to common codebase

---
+++ low_usb-controller/RTOSDemo/fft/fix_fft.h
@@ -0,0 +1,6 @@
+#ifndef __FIX_FFT_H__
+#define __FIX_FFT_H__
+
+int fix_fftr(short f[], int m, int inverse)
+
+#endif //__FIX_FFT_H__
---
+++ low_usb-controller/RTOSDemo/locator/locator_task.h
@@ -0,0 +1,15 @@
+#ifndef __LOCATOR_TASK_H__
+#define __LOCATOR_TASK_H__
+
+typedef struct _locatorstat
+{
+  char state;
+  unsigned short data[128];
+  unsigned short position;
+} locatorstat;
+
+locatorstat locstat;
+
+void vStartLocatorTask( unsigned portBASE_TYPE uxPriority );
+
+#endif
